a4e65a4548f3a1ae1b7e4bfe24a520a9d6126dcc
Bianca Gallicchio <94980037+BiancaGallicchio@users.noreply.github.com>
2022-07-30 20:32:19 -0300

    imprimir pré-ordem

---
+++ pre-ordem.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+  int data;
+  Node *left;
+  Node *right;
+};
+
+Node *novoNode(int data) {
+  Node *newNode = new Node();
+  newNode->data = data;
+  newNode->left = NULL;
+  newNode->right = NULL;
+  return newNode;
+}
+
+Node *insert(Node *root, int data) // O(logn)
+{
+  if (root == NULL) {
+    root = novoNode(data); 
+  } else if (data <= root->data) {
+    root->left = insert(root->left, data);
+  } else {
+    root->right = insert(root->right, data);
+  }
+  return root;
+}
+
+void preOrder(Node *root) // O(n)
+{
+  if (root == NULL)
+    return;
+  cout << root->data << " "; // imprimo o primeiro
+  preOrder(root->left);      // recursão, começo com o da esquerda
+  preOrder(root->right);
+}
+
+int main()
+{
+  Node *root = NULL;
+  
+  insert(root, 10); insert(root, 15); insert(root, 5); insert(root, 4); insert(root, 8); insert(root, 12);
+  preOrder(root);
+  
+}
