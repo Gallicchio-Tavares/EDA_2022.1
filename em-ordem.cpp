b6df7e6bcf2e499aa61bd975e4530fd513d81242
Bianca Gallicchio <94980037+BiancaGallicchio@users.noreply.github.com>
2022-07-30 20:33:32 -0300

    imprimir em ordem

---
+++ em-ordem.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+  int data;
+  Node *left;
+  Node *right;
+};
+
+Node *novoNode(int data) {
+  Node *newNode = new Node();
+  newNode->data = data;
+  newNode->left = NULL;
+  newNode->right = NULL;
+  return newNode;
+}
+
+Node *insert(Node *root, int data) // O(logn)
+{
+  if (root == NULL) {
+    root = novoNode(data); 
+  } else if (data <= root->data) {
+    root->left = insert(root->left, data);
+  } else {
+    root->right = insert(root->right, data);
+  }
+  return root;
+}
+
+void inOrder(Node *root) // O(n)
+{
+  if (root == NULL)
+    return;
+  inOrder(root->left); // recursão, começo com o da esquerda
+  cout << root->data << " ";
+  inOrder(root->right);
+}
+
+int main()
+{
+  Node *root = NULL;
+  
+  insert(root, 10); insert(root, 15); insert(root, 5); insert(root, 4); insert(root, 8); insert(root, 12);
+  inOrder(root);
+}
